f347525c851ef39b9f58da775367dcd8465091fc
Ferran Vidal <ferran.vidal@letsbonus.com>
2013-03-07 12:11:56 +0100

    Fix deal API

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,18 +1,13 @@
 class DealsController < ApplicationController
   def index
-    if params[:id].present?
-      detail
+    if params[:city_id].present?
+      @deals = City.find(params[:city_id]).deals.running_now.includes(:deal_images, :options, :city => :country).to_json(:include => [:deal_images, :options])
+    elsif params[:id].present?
+      @deals = Deal.includes(:deal_images, :options, :city => :country).find(params[:id]).to_json(:include => [:deal_images, :options])
     else
-      list
+      @deals = Deal.includes(:deal_images, :options, :city => :country).all().to_json(:include => [:deal_images, :options])
     end
-  end
-
-  protected
-  def list
-    render :json => City.find(params[:city_id]).deals.running_now.includes(:deal_images, :options, :city => :country).to_json(:include => [:deal_images, :options])
-  end
-  
-  def detail
-    render :json => Deal.includes(:deal_images, :options, :city => :country).find(params[:id]).to_json(:include => [:deal_images, :options])
+    
+    render :json => @deals
   end
 end
