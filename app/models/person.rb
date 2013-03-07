d01a06ff140b7598463bfc33fbe1a7c23e6457de
Mihai Bogdan Racovita <mihai.bogdan@letsbonus.com>
2013-03-07 14:12:49 +0100

    Adding attributes to person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,7 @@ class Person < ActiveRecord::Base
 
   has_secure_password
 
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :gender , :born_on, :city_id
 
   validates_presence_of :email
 end
