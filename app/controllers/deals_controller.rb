0779d44941c7702b390efcc8e21c2de5b5b090e6
Ferran Vidal <ferran.vidal@letsbonus.com>
2013-03-07 11:27:25 +0100

    Fix detail controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,10 +1,18 @@
 class DealsController < ApplicationController
   def index
-    city = City.find(params[:city_id])
-    render :json => city.deals.running_now.includes(:deal_images, :options, :city => :country).to_json(:include => [:deal_images, :options])
+    if params[:id].present?
+      detail
+    else
+      list
+    end
   end
 
-  def show
+  protected
+  def list
+    render :json => City.find(params[:city_id]).deals.running_now.includes(:deal_images, :options, :city => :country).to_json(:include => [:deal_images, :options])
+  end
+  
+  def detail
     render :json => Deal.includes(:deal_images, :options, :city => :country).find(params[:id]).to_json(:include => [:deal_images, :options])
   end
 end
